12aef4bf1c2ba25bdf992abf4d8b909d0b348fc9
Blair B <blairbcodes@gmail.com>
2022-08-12 23:31:00 -0230

    planned file layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NotFound from './pages/404'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Home from './pages/home'
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
